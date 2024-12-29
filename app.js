39139e219cc85658c067953a896f79bb2fd692d9
Saulo Eduardo <saulo.eduita@gmail.com>
2024-12-29 17:23:57 -0300

    Revert "Deixando o jogo mais fácil"
    This reverts commit 6e8c8c7a92a6cf71f8036a2609f209f1ec8fc015.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 50;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 0;
 
@@ -10,7 +10,7 @@ function exibirTextoNaTela(tag,texto){
 }
 
 exibirTextoNaTela('h1', 'Jogo do número secreto');
-exibirTextoNaTela('p', `escreva um número de 1 a ${numeroLimite}`);
+exibirTextoNaTela('p', 'escreva um número de 1 a 100');
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
